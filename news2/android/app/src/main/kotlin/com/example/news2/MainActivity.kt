f2115315d750ab4b6689327ebb629cc9391dc9c7
DALIAR2GAB <152261531+DALIAR2GAB@users.noreply.github.com>
2025-11-01 20:06:57 +0200

    Delete news2 directory

--- news2/android/app/src/main/kotlin/com/example/news2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.news2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- news2/lib/models/newsmodel.dart
+++
@@ -1,8 +0,0 @@
-class Newsmodel{
-  //new commit in 
-  List<dynamic> artcicls;
-  Newsmodel({required this.artcicls});
-  factory Newsmodel.fromjson(Map<String,dynamic>json){
-    return Newsmodel(artcicls: json['articles']);
-  }
-}
